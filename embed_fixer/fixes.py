130bf652d850e759a32d1dee0779dde3e589dc09
seria <seria.ati@gmail.com>
2026-07-29 22:40:17 +0900

    refactor(threads): remove now invalid embedez fix method

--- embed_fixer/fixes.py
+++ embed_fixer/fixes.py
@@ -462,16 +462,6 @@ DOMAINS: Final[list[Domain]] = [
                 repo_url="https://github.com/everettsouthwick/vxThreads",
             ),
             FixMethod(
-                id=30,
-                name=EMBEDEZ_NAME,
-                fixes=[
-                    ReplaceFix(old_domain="threads.net", new_domain="threadsez.com"),
-                    ReplaceFix(old_domain="threads.com", new_domain="threadsez.com"),
-                ],
-                repo_url=EMBEDEZ_REPO_URL,
-                has_ads=True,
-            ),
-            FixMethod(
                 id=33,
                 name="FixEmbed",
                 fixes=[AppendURLFix(domain="fixembed.app/embed")],
